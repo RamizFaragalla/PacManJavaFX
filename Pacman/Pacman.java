47e4c84c60865168638c16c81b276bf5cd69424c
[Ramiz Faragalla] <[ramezkelada66@gmail.com]>
2020-05-01 17:01:04 -0400

    Ramiz

--- Pacman/Pacman.java
+++ Pacman/Pacman.java
@@ -1,4 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 
-public class Pacman {
-
-}
+public class Pacman extends Application{
+	public static void main(String[] args) {
+	      // Launch the application.
+	      launch(args);
+	}
+	
+	@Override
+   /**
+    * @param primaryStage
+    * @return void
+    */
+	public void start(Stage primaryStage) {
+		 // Set the stage title.
+	      primaryStage.setTitle("Pacman");
+	      
+	      // Show the window.
+	      primaryStage.show();
+	}
+}	
